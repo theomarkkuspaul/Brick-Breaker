11c5b272c171d00183f1fc011fec18d9934e1b10
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-06-03 20:13:52 -0400

    Update game js file

--- js/game.js
+++ js/game.js
@@ -25,10 +25,6 @@ $(document).ready(function(){
     "layer1": [new Brick(), new Brick(140), new Brick(230), new Brick(320), new Brick(410), new Brick(500), new Brick(590), new Brick(680)],
     "layer2":[new Brick(50, 80),new Brick(140, 80), new Brick(230, 80), new Brick(320, 80), new Brick(410, 80), new Brick(500, 80), new Brick(590, 80), new Brick(680, 80)]
    }
-  Brick.prototype.drawBrick = function(){
-    ctx.fillRect(this.x,this.y,this.width,this.height);
-    ctx.fillStyle = this.colour;
-  }
 
 
   var move = function(){
