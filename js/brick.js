2e46fd8f84ae739bfa87377b8332fb7e4740ae8a
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-06-03 20:13:21 -0400

    Remove Brick Draw function from game js file, place in Brick js

--- js/brick.js
+++ js/brick.js
@@ -6,3 +6,8 @@ var Brick = function(x, y){
   this.width = 80,
   this.colour = "blue"
 }
+
+Brick.prototype.drawBrick = function(){
+  ctx.fillRect(this.x,this.y,this.width,this.height);
+  ctx.fillStyle = this.colour;
+}
