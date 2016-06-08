fcb83e0f894e6f68ad22eb7544c4fc96fca60314
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2016-06-08 17:17:47 -0400

    Create Game Function that returns the current level

--- js/models/game.js
+++ js/models/game.js
@@ -2,6 +2,7 @@ function Game(ball, player, paddle) {
   this.player = player,
   this.ball = ball,
   this.paddle = paddle,
+  this.startingLevel = 0, //when a new game starts, default to first level
   this.levels = [
     new LevelOne(),
     new LevelTwo(),
@@ -10,6 +11,22 @@ function Game(ball, player, paddle) {
   ]
   this.bricks = this.levels[0].bricks // loads first level
 }
+
+Game.prototype.currentLevel = function () {
+  var completedLevels = 0
+  for ( var levelIdx in this.levels ) {
+    var levelBricksDestroyed = 0;
+    for ( var brickIdx = 0; brickIdx < this.levels[levelIdx].bricks.length; brickIdx++ ){
+      if ( this.levels[levelIdx].bricks[brickIdx] == null){
+        levelBricksDestroyed += 1
+      }
+      if ( levelBricksDestroyed == this.levels[levelIdx].bricks.length){
+        completedLevels += 1
+      }
+    }
+  }
+  return completedLevels + 1;
+};
 
 // Game.prototype.loadLevel = function (level) {
 //   return level.bricks
